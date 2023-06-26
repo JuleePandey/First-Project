229aaaedefc5905aed45e2295d5f644a87ea3df8
Julee Pandey <juleepandey96@gmail.com>
2023-06-26 12:07:31 +0530

    update code

---
+++ javascript/script.js
@@ -0,0 +1,18 @@
+
+
+/*// Function to get random elements from an array
+function getRandomElements(arr, numElements) {
+// Shuffle the array using Fisher-Yates algorithm
+for (let i = arr.length - 1; i > 0; i--) {
+const j = Math.floor(Math.random() * (i + 1));
+[arr[i], arr[j]] = [arr[j], arr[i]];
+}
+
+// Return the first numElements elements
+return arr.slice(0, numElements);
+}
+
+// Get three random elements from the names array
+const randomElements = getRandomElements(names, 3);
+console.log(randomElements)*/
+
---
+++ jawed-habib/jawed-habib/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/owl.carousel.min.css">
+    <title>Jawed Habibi</title>
+</head>
+<body>
+
+<div class="header">
+            <div class="container">
+                <div class="d-flex justify-content-between align-items center">
+                    <div>
+                        <img src="assets/images/jh-logo.png" alt="">
+                    </div>
+                    <div>
+                        <ul>
+                            <li><a href="">ACADEMY</a></li>
+                            <li style="color: #C80406;"><a href="">FRANCHISE</a></li>
+                            <li><a href="">SERVICES</a></li>
+                            <li><a class="header-btn" href="">STORE LOCATION</a></li>
+                            <li><img src="assets/images/bar-icon.png" alt=""></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
